20787d58a31600a8b03f41341b510214ac067b03
Jonathan Heaney <jonathan.heaney@gmail.com>
2023-07-28 20:44:22 -0500

    Added functionality to filter results by occupation

--- pantheon-react/src/PeopleList.js
+++ pantheon-react/src/PeopleList.js
@@ -1,60 +1,16 @@
-// import React, { useState } from 'react';
-// import styles from './PeopleList.module.css'; // Import the CSS module
-
-// const PeopleList = () => {
-//   const [year, setYear] = useState('');
-//   const [people, setPeople] = useState([]);
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     const response = await fetch(`http://localhost:3001/people/${year}`);
-//     const data = await response.json();
-//     setPeople(data);
-//   };
-
-//   return (
-//     <div>
-//       <form onSubmit={handleSubmit} className={styles.formContainer}>
-//         <input
-//           label="Year"
-//           type="number"
-//           value={year}
-//           onChange={(e) => setYear(e.target.value)}
-//           className={styles.yearInput}
-//         />
-//         <button type="submit" className={styles.submitButton}>
-//           Submit
-//         </button>
-//       </form>
-//       <div className={styles.peopleList}>
-//         {people.map((person) => (
-//           <div key={person.id} className={styles.person}>
-//             <h2>{person.name}</h2>
-//             <p>{person.occupation}</p>
-//             <p>Born in {person.birthyear}</p>
-//             <p>Fame score: {person.hpi}</p>
-//             <p>
-//               Age in {year}: {person.age}
-//             </p>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default PeopleList;
-
 import React, { useState } from 'react';
 import styles from './PeopleList.module.css';
 
 const PeopleList = () => {
   const [year, setYear] = useState('');
+  const [occupation, setOccupation] = useState('');
   const [people, setPeople] = useState([]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch(`http://localhost:3001/people/${year}`);
+    const response = await fetch(
+      `http://localhost:3001/people/${year}?occupation=${occupation}`
+    );
     const data = await response.json();
     setPeople(data);
   };
@@ -74,6 +30,13 @@ const PeopleList = () => {
           onChange={(e) => setYear(e.target.value)}
           className={styles.yearInput}
           placeholder="Enter a year"
+        />
+        <input
+          type="text"
+          value={occupation}
+          onChange={(e) => setOccupation(e.target.value)}
+          className={styles.occupationInput}
+          placeholder="Enter an occupation"
         />
         <button type="submit" className={styles.submitButton}>
           Submit
--- pantheon-server/app.js
+++ pantheon-server/app.js
@@ -1,25 +1,36 @@
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+const pgp = require('pg-promise')();
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
 
-app.get('/people/:year', async (req, res) => {
-  const year = req.params.year;
-  const result = await pool.query(`
-        SELECT *, ${year} - birthyear as age 
-        FROM pantheon 
-        WHERE birthyear <= ${year} AND (deathyear >= ${year} OR deathyear IS NULL) 
-        ORDER BY hpi DESC 
-        LIMIT 10
-    `);
-  res.json(result.rows);
+const db = pgp({
+  connectionString: process.env.DATABASE_URL,
 });
 
-app.listen(3001, () => console.log('Server is running on port 3001'));
+app.get('/people/:year', async (req, res) => {
+  const year = parseInt(req.params.year);
+  const occupation = req.query.occupation;
+  let query = `SELECT *, ${year} - birthyear AS age FROM pantheon WHERE birthyear <= $1 AND (deathyear >= $1 OR deathyear IS NULL) ORDER BY hpi DESC LIMIT 10`;
+  let values = [year];
+
+  if (occupation) {
+    query = `SELECT *, ${year} - birthyear AS age FROM pantheon WHERE LOWER(occupation) LIKE LOWER('%' || $2 || '%') AND birthyear <= $1 AND (deathyear >= $1 OR deathyear IS NULL) ORDER BY hpi DESC LIMIT 10`;
+    values.push(occupation);
+  }
+
+  try {
+    const result = await db.any(query, values);
+    res.json(result);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+const port = 3001;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
